8813777fa878cef3a097ac2829596c80ce090215
tuckerz <thzischka@ucdavis.edu>
2021-06-14 17:21:46 -0700

    CA - Added Function Functionality
    Able to Call averaging function and call anyone of the collumns from the dataset.  Need to look more closely at the data returned. The averaged data being returned isn't exactly right as it isn't dividing it evenly into all bins.

--- Cell Analysis/CellAnalysis.m
+++ Cell Analysis/CellAnalysis.m
@@ -1,14 +1,22 @@
 %% Cell Analysis (CA)
 %   Written by Tucker Zischka
 %   6/14/2021
-% This will help to solve for resistance in the busbar/bolted joints of the
-% accumulator 
+% This will help to analyze the data captured by the Senior Design Project
 
-clc; clear; close all;
+clc; clf; clear; close all;
+
 %% Data Import
 
 % 4x.75 ohms = .19
-addpath('.\Cell Data')
+addpath("./Cell Data");
+
+%filesStructure = dir(fileFolder)
+%allFileNames = {filesStructure(:).name}
+%for k = 1 : length(allFileNames)
+%    importfile("Cell2")
+%    allFileNames{k}
+%end
+
 test1.cell2 = importfile("Cell2 4 .75ohm.csv", [3, Inf]);
 test1.cell3 = importfile("Cell3 4 .75ohm.csv", [3, Inf]);
 test1.cell4 = importfile("Cell4 4 .75ohm.csv", [3, Inf]);
@@ -19,10 +27,14 @@ test1.cell8 = importfile("Cell8 4 .75ohm.csv", [3, Inf]);
 test1.cell9 = importfile("Cell9 4 .75ohm.csv", [3, Inf]);
 test1.cell10 = importfile("Cell10 4 .75ohm.csv", [3, Inf]);
 
+%% Calculate Call
+
+averageShuntMV = calculateAvg(test1, 'VShuntmV');
+averageBatMV = calculateAvg(test1, 'VbatValmV');
+
+%% Functions
+function testSet = calculateAvg(test1 , dataName)
 fn = fieldnames(test1);
-for i = 1:numel(fn)  
-    %test2.(fn{i}) = test1.(fn{i});
-end 
 
 %% Preprocess Time
 for i = 1:numel(fn)
@@ -44,7 +56,7 @@ end
 %Dump everything into one 
 totalCurrent = [];
 for i = 1:numel(fn)
-    totalCurrent = cat(1, totalCurrent, [test1.(fn{i}).Times, test1.(fn{i}).VShuntmV]);
+    totalCurrent = cat(1, totalCurrent, [test1.(fn{i}).Times, test1.(fn{i}).(dataName)]);
 end
 
 % Sort the items
@@ -53,6 +65,9 @@ totalCurrent = sortrows(totalCurrent);
 % Remove NaNs
 totalCurrent = rmmissing(totalCurrent);
 
+% Regression Fit 
+regressionFit = fit(totalCurrent(:,1), totalCurrent(:,2), 'gauss8');
+
 % Bin the Items
 totalBins = 1000; 
 [bins, edges] = discretize(totalCurrent, totalBins);
@@ -60,7 +75,7 @@ totalBins = 1000;
 summedCurrent = zeros([totalBins , 2]); 
 old = bins(1);
 numberToAverage = 0; 
-for i = 1:numel(bins(:,1))    %iteraterate through the entire sorted set
+for i = 1:numel(bins(:,1))                                                      %iteraterate through the entire sorted set
     summedCurrent(bins(i),:) = summedCurrent(bins(i),:) + totalCurrent(i,:);    % Sum the sorted set together in the bins they belong to
     numberToAverage = numberToAverage + 1;                                      % Keep track of how much to average
     if old ~= bins(i)                                                           % Average them once you reached the end of the bing
@@ -70,27 +85,25 @@ for i = 1:numel(bins(:,1))    %iteraterate through the entire sorted set
     end
 end
 
+
 %% Graph The Average Current
 
-% Plot Properties
-width = 3;     % Width in inches
-height = 3;    % Height in inches
-alw = 0.75;    % AxesLineWidth
-fsz = 11;      % Fontsize
-lw = 1.5;      % LineWidth
-msz = 8;       % MarkerSize
-
-figure(1);
-pos = get(gcf, 'Position');
+figure;
+%set(gca, 'FontSize', fsz, 'LineWidth', alw); %<- Set properties
+plot(regressionFit);
 %set(gcf, 'Position', [pos(1) pos(2) width*100, height*100]); %<- Set size
-set(gca, 'FontSize', fsz, 'LineWidth', alw); %<- Set properties
-plot(summedCurrent(:,1),summedCurrent(:,2), 'LineWidth',lw,'MarkerSize',msz); %<- Specify plot properites
+
+%plot(summedCurrent(:,1),summedCurrent(:,2), 'LineWidth',lw,'MarkerSize',msz); %<- Specify plot properites
 xlim([1 547]);
 xlabel('Time [Seconds]');
-ylabel('Shunt Voltage [milliVolts]');
-title('Average Shunt Voltage vs Time');
+ylabel('Voltage [milliVolts]');
+title(strcat({'Average '}, dataName, {' vs Time'}));
+hold on; 
 
-%% Functions
+%% Function Return 
+testSet = summedCurrent; 
+end
+
 function cell2475ohm1 = importfile(filename, dataLines)
 %% Input handling
 
